82797bfccdc1ce0c010bd9ce051b4a1bcf385014
Dom <domportera@gmail.com>
2023-03-10 21:24:05 -0500

    remove NiceTouch for submodule replacement

--- NiceTouch/UnitConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace NiceTouch
-{
-    public static class UnitConstants
-    {
-        public const float InchesToCmF = 2.54f;
-        public const double InchesToCmD = 2.54d;
-        public const int CmToMm = 10;
-    }
-}
